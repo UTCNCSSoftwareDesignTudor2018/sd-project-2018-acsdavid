4f1ca466f2321bc8b610c76783b3c3ccc0f4bc5c
Ács Dávid <acsdavid97@gmail.com>
2018-04-22 21:45:44 +0300

    reusable library test

---
+++ code/BikePortalTests/DataAccess/BikePortalDbContextTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BikePortal.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikePortal.DataAccess.Tests
+{
+    [TestClass()]
+    public class BikePortalDbContextTests
+    {
+        [TestMethod()]
+        public void BikePortalDbContextTest()
+        {
+            var dbContext = new BikePortalDbContext();
+            Database.SetInitializer(new BikePortalDbTestInitializer());
+            var users = dbContext.Users.ToList();
+            Assert.IsTrue(users.Count > 0);
+        }
+    }
+}
